a7ff1352520f87e213fe0fdb9c1cbafc89e22d9a
EC2 Default User <ec2-user@ip-172-31-30-82.ec2.internal>
2022-10-18 14:36:21 +0000

    Start 3-kadai

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(@book)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ db/migrate/20221016080538_create_books.rb
@@ -0,0 +1,9 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title #本のタイトル
+      t.string :body #感想
+      t.timestamps
+    end
+  end
+end
